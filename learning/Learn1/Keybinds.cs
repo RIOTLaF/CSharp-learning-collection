c04113bf492d4201e220af754a4906a8c32940e8
RIOTLaf <tavaresnicolas157@gmail.com>
2025-07-28 22:00:47 -0300

    Initial commit

---
+++ learning/Learn1/Keybinds.cs
@@ -0,0 +1,25 @@
+namespace learning.Learn1;
+
+public class Class
+{
+     static void DetectKeybinds()
+     {
+          Console.WriteLine("Pizza is better than hamburger do you agree? (Y/N)\n");
+          bool gotreply = false;
+          while (!gotreply)
+          {
+               Thread.Sleep(200);
+               var key = Console.ReadKey(true);
+               if (key.Key == ConsoleKey.Y & key.Modifiers == ConsoleModifiers.None)
+               {
+                    Console.WriteLine("Thanks for agreeing with me");
+                    gotreply = true;
+               }
+               else if (key.Key == ConsoleKey.N & key.Modifiers == ConsoleModifiers.None)
+               {
+                    Console.WriteLine("I Thought you was my friend...");
+                    gotreply = true;
+               }
+          }
+     }
+}
